367fc38520bfcfc3faeeb767433dd0f7a17ec5ce
Mekontso Anderson Sa'a <mekontsoanderson@gmail.com>
2021-07-12 10:19:38 +0100

    User registration

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Public routes
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route group for authenticated users only
+Route::middleware(['auth:api'])->group(function (){
+
 });
+
+// Route group for guests
+Route::middleware(['guest:api'])->group(function (){
+    Route::post('register',[RegisterController::class,'register']);
+});
+
